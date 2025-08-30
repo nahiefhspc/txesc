e57ee400907f1722dfa98166d936a1392bcbd80c
nahiefhspc <t.al.khackheist@gmail.com>
2025-08-30 15:55:09 +0530

    Update saini.py

--- saini.py
+++ saini.py
@@ -228,7 +228,7 @@ def time_name():
     return f"{date} {current_time}.mp4"
 
 async def download_video(url,cmd, name):
-    download_cmd = f'{cmd} -R 25 --fragment-retries 25 --external-downloader aria2c --downloader-args "aria2c: -x 32 -j 64"'
+    download_cmd = f'{cmd} -R 25 --fragment-retries 25 --external-downloader aria2c --downloader-args "aria2c: -x 16 -j 32"'
     global failed_counter
     print(download_cmd)
     logging.info(download_cmd)
